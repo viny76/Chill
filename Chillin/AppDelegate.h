aeca12d630a3c02a440ad4b017e1bd57f842cb34
viny76 <jardel.vincent@gmail.com>
2016-09-16 17:09:08 +0200

    Initial Commit

---
+++ Chillin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chillin
+//
+//  Created by Vincent Jardel on 16/09/2016.
+//  Copyright © 2016 Vincent Jardel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chillin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Chillin
+//
+//  Created by Vincent Jardel on 16/09/2016.
+//  Copyright © 2016 Vincent Jardel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
